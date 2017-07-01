154963b7266afb32f63115538ee45e3ce59f58dd
Christian Kauten <Kautenja@users.noreply.github.com>
2017-07-01 01:46:44 -0500

    Update SpinerLayer.swift
    * refactor instances of M_PI to CGfloat.pi and Double.pi where used

--- SubmitTransition/Classes/SpinerLayer.swift
+++ SubmitTransition/Classes/SpinerLayer.swift
@@ -15,10 +15,14 @@ class SpinerLayer: CAShapeLayer {
         let radius:CGFloat = (frame.height / 2) * 0.5
         self.frame = CGRect(x: 0, y: 0, width: frame.height, height: frame.height)
         let center = CGPoint(x: frame.height / 2, y: bounds.center.y)
-        let startAngle = 0 - M_PI_2
-        let endAngle = M_PI * 2 - M_PI_2
+        let startAngle = 0 - CGFloat.pi
+        let endAngle = CGFloat.pi * 2 - CGFloat.pi
         let clockwise: Bool = true
-        self.path = UIBezierPath(arcCenter: center, radius: radius, startAngle: CGFloat(startAngle), endAngle: CGFloat(endAngle), clockwise: clockwise).cgPath
+        self.path = UIBezierPath(arcCenter: center,
+                                 radius: radius,
+                                 startAngle: startAngle,
+                                 endAngle: endAngle,
+                                 clockwise: clockwise).cgPath
 
         self.fillColor = nil
         self.strokeColor = spinnerColor.cgColor
@@ -36,7 +40,7 @@ class SpinerLayer: CAShapeLayer {
         self.isHidden = false
         let rotate = CABasicAnimation(keyPath: "transform.rotation.z")
         rotate.fromValue = 0
-        rotate.toValue = M_PI * 2
+        rotate.toValue = Double.pi * 2
         rotate.duration = 0.4
         rotate.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
         
